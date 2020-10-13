7e69115abbceb9aeccedcd281c3e4c135cbce333
Nikhil Kamat <nikhilkamat@Nikhils-MacBook-Pro.local>
2020-10-13 22:01:53 +0530

    created a function which extracts column names from an html table #2

--- scrape_wikipedia/utils/web_scraping.py
+++ scrape_wikipedia/utils/web_scraping.py
@@ -1,11 +1,11 @@
 import requests
-from bs4 import BeautifulSoup
+import bs4
 
 def get_soup(url):
     # Request the document
     r = requests.get(url)
     html_doc = r.text
-    soup = BeautifulSoup(html_doc,'lxml')
+    soup = bs4.BeautifulSoup(html_doc,'lxml')
     return soup
 
 
@@ -16,8 +16,48 @@ def get_tables_from_url(soup, attrs:dict):
 
     attrs: class attributes
     """
-    assert (isinstance(soup, BeautifulSoup))
+    assert (isinstance(soup, bs4.BeautifulSoup))
     # Request the document
     tables = soup.findAll("table", attrs)
 
     return tables
+
+
+# ---------------------------------------------------
+
+def get_column_names(my_table)-> list:
+    '''
+    Extract the column names from a wikitable or any html table.
+
+    Params:
+        my_table: a bs4 object representing an html table.
+
+    Returns:
+        a list of column names.
+    ----
+    Note: This is a well documented function!!!
+    '''
+    assert isinstance(my_table, bs4.element.Tag)
+
+    # "th" is a header cell
+    header_rows = my_table.find_all('th')
+
+    """
+    all wiki tables should have header rows('th')
+    but what if it doesn't?
+    here is a method to make if fool proof
+    """
+
+    if len(header_rows) == 0:
+        #'tr' is a row
+        all_rows = my_table.find_all('tr')
+        #if the table is empty, return an empty list
+        if len(all_rows) == 0:
+            return []
+        header_rows = all_rows[0]
+
+    # clean up text inside cell
+    # this method does not change the casing of the column text
+    column_names = [x.text.strip() for x in header_rows]
+
+    return column_names
