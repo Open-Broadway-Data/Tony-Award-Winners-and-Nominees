bd9069270b9a97a8e78929d43830df173df73b96
ybressler <yaakovbressler@gmail.com>
2021-01-15 11:29:19 -0500

    This is a commit.

--- tests/scratch_template.py
+++ tests/scratch_template.py
@@ -13,12 +13,18 @@ pd.options.display.max_rows = 100
 wq = WikiScraper(url=None)
 data = wq.get_all_tony_award_data(save=True)
 
+
+
 # Now you can reference individual items in the data and build a dataframe for it.
 # Very useful for testing data quality.
 for key, value in data.items():
+	print(key)
+	continue
 	df = pd.DataFrame(value)
 	if 'award_name' not in df.columns:
 		df['award_name'] = key
 	break
 
 df.head(5)
+
+# foo
